0866e0681530c4f84658f58e7b42afeb3ae8444a
PlexILIghT <liexa2024@gmail.com>
2025-01-24 22:49:13 +1000

    edit req and settings.py

--- djangoProject/zenithFanclub/settings.py
+++ djangoProject/zenithFanclub/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
+
+SECRET_KEY='django-insecure-+*z7*5*nh2xh-zg49jwgod@&hxl*tg)(x^tdbs7ljg_7a3)y8o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
