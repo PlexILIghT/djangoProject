4e60e5df5ad20d68e25c0f61811ce6437d848fe5
PlexILIghT <liexa2024@gmail.com>
2025-01-26 03:01:52 +1000

    a bit of polishing

--- djangoProject/user/urls.py
+++ djangoProject/user/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('profile/', profile_view, name='profile'),
     path('delete/', delete_user_view, name='delete_user'),
-    path('logout', logout_view, name="logout_view")
+    path('logout/', logout_view, name="logout_view")
 ]
